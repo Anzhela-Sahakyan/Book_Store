67a2e561d2745deed55499bef2b1d2ac2e8bf1ec
Anzhela <sahakyananzhela@gmail.com>
2023-08-28 18:23:30 +0400

    feat: initializing the project

---
+++ src/Components/Header.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Header() {
+  return (
+    <Box>
+      <Typography>BOOK STORE</Typography>
+    </Box>
+  );
+}
---
+++ src/Types/types.tsx
@@ -0,0 +1,7 @@
+export default interface Book {
+  name: string;
+  description: string;
+  price: number;
+  discount?: number;
+  img: string;
+}
